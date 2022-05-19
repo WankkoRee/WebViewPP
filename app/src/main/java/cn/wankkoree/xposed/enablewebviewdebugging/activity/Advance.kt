02e60b01e23d48e338198a3527bf90d20ff09572
Wankko Ree <wkr@wkr.moe>
2022-05-19 21:33:04 +0800

    feat: 更改高级界面显示方式

--- app/src/main/java/cn/wankkoree/xposed/enablewebviewdebugging/activity/Advance.kt
+++ app/src/main/java/cn/wankkoree/xposed/enablewebviewdebugging/activity/Advance.kt
@@ -2,20 +2,17 @@ package cn.wankkoree.xposed.enablewebviewdebugging.activity
 
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.viewpager.widget.PagerAdapter
 import cn.wankkoree.xposed.enablewebviewdebugging.BuildConfig
 import cn.wankkoree.xposed.enablewebviewdebugging.R
-import cn.wankkoree.xposed.enablewebviewdebugging.activity.component.License
 import cn.wankkoree.xposed.enablewebviewdebugging.databinding.AdvanceBinding
 import com.highcapable.yukihookapi.YukiHookAPI
-import com.tmall.ultraviewpager.UltraViewPager
-import com.tmall.ultraviewpager.transformer.UltraDepthScaleTransformer
 
 class Advance: AppCompatActivity() {
     private lateinit var viewBinding: AdvanceBinding
@@ -26,42 +23,39 @@ class Advance: AppCompatActivity() {
         viewBinding = AdvanceBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        viewBinding.advanceLicense.apply {
-            setScrollMode(UltraViewPager.ScrollMode.HORIZONTAL)
-            setMultiScreen(0.75f)
-            setPageTransformer(false, UltraDepthScaleTransformer())
-            adapter = LicenseAdapter(arrayListOf<View>(
-                License(this@Advance).apply {
-                    icon = AppCompatResources.getDrawable(this@Advance, R.mipmap.wankkoree)
-                    title = getString(R.string.wankko_ree)
-                    desc = getString(R.string.author_of_this_module)
-                    color = getPrimaryColor(icon!!, this@Advance).first
-                    setOnClickListener {
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WankkoRee")))
-                    }
-                },
-                License(this@Advance).apply {
-                    icon = AppCompatResources.getDrawable(this@Advance, R.mipmap.yuki_hook_api)
-                    title = getString(R.string.yuki_hook_api)
-                    desc = getString(R.string.this_module_is_constructed_using_yukihookapi) + "\n" + getString(R.string.advance_yuki_version_text).format(YukiHookAPI.API_VERSION_NAME, YukiHookAPI.API_VERSION_CODE)
-                    color = getPrimaryColor(icon!!, this@Advance).first
-                    setOnClickListener {
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/fankes/YukiHookAPI")))
-                    }
-                },
-                License(this@Advance).apply {
-                    icon = packageManager.getApplicationIcon(BuildConfig.APPLICATION_ID)
-                    title = getString(R.string.app_name)
-                    desc = getString(R.string.app_description)
-                    color = getPrimaryColor(icon!!, this@Advance).first
-                    setOnClickListener {
-                        toast?.cancel()
-                        toast = Toast.makeText(this@Advance, getString(R.string.if_you_find_this_project_useful_please_star_this_project), Toast.LENGTH_SHORT)
-                        toast!!.show()
-                        startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WankkoRee/EnableWebViewDebugging")))
-                    }
-                },
-            ))
+        getPrimaryColor(viewBinding.advanceLicenseAuthorIcon.drawable, this@Advance).first.also {
+            viewBinding.advanceLicenseAuthorCard.backgroundTintList = colorStateSingle((it or 0xff000000.toInt()) and 0x33ffffff)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) viewBinding.advanceLicenseAuthorCard.outlineSpotShadowColor = it
+        }
+        viewBinding.advanceLicenseAuthorButtonGithub.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WankkoRee")))
+        }
+        viewBinding.advanceLicenseAuthorButtonEmail.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("mailto://wkr@wkr.moe")))
+        }
+
+        viewBinding.advanceLicenseYukiDesc.text = getString(R.string.this_module_is_constructed_using_yukihookapi) + "\n" + getString(R.string.advance_yuki_version_text).format(YukiHookAPI.API_VERSION_NAME, YukiHookAPI.API_VERSION_CODE)
+        getPrimaryColor(viewBinding.advanceLicenseYukiIcon.drawable, this@Advance).first.also {
+            viewBinding.advanceLicenseYukiCard.backgroundTintList = colorStateSingle((it or 0xff000000.toInt()) and 0x33ffffff)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) viewBinding.advanceLicenseYukiCard.outlineSpotShadowColor = it
+        }
+        viewBinding.advanceLicenseYukiButtonGithub.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/fankes/YukiHookAPI")))
+        }
+
+        viewBinding.advanceLicenseModuleIcon.setImageDrawable(packageManager.getApplicationIcon(BuildConfig.APPLICATION_ID))
+        getPrimaryColor(viewBinding.advanceLicenseModuleIcon.drawable, this@Advance).first.also {
+            viewBinding.advanceLicenseModuleCard.backgroundTintList = colorStateSingle((it or 0xff000000.toInt()) and 0x33ffffff)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) viewBinding.advanceLicenseModuleCard.outlineSpotShadowColor = it
+        }
+        viewBinding.advanceLicenseModuleButtonGithub.setOnClickListener {
+            toast?.cancel()
+            toast = Toast.makeText(this@Advance, getString(R.string.if_you_find_this_project_useful_please_star_this_project), Toast.LENGTH_SHORT)
+            toast!!.show()
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/WankkoRee/EnableWebViewDebugging")))
+        }
+        viewBinding.advanceLicenseModuleButtonTelegram.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/EnableWebViewDebugging")))
         }
 
         viewBinding.advanceToolbarBack.setOnClickListener {
