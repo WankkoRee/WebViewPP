e500a72459a0305cf1020e96932e330842b1830a
Wankko Ree <wkr@wkr.moe>
2023-05-08 12:28:14 +0800

    feat: 完善 #33

--- app/src/main/java/cn/wankkoree/xp/webviewpp/activity/App.kt
+++ app/src/main/java/cn/wankkoree/xp/webviewpp/activity/App.kt
@@ -278,7 +278,7 @@ class App : AppCompatActivity() {
                 menuInflater.inflate(R.menu.app_toolbar_menu, menu)
                 with(prefs("apps_$pkg")) {
                     menu.findItem(R.id.app_toolbar_menu_debug_mode).isChecked = get(AppSP.debug_mode)
-                    menu.findItem(R.id.app_toolbar_menu_byass_packer).isChecked = get(AppSP.bypass_packer)
+                    menu.findItem(R.id.app_toolbar_menu_app_is_protected).isChecked = get(AppSP.app_is_protected)
                 }
                 setOnMenuItemClickListener { menuItem ->
                     when (menuItem.itemId) {
@@ -301,8 +301,8 @@ class App : AppCompatActivity() {
                                 }.show()
                             }
                         }
-                        R.id.app_toolbar_menu_byass_packer -> {
-                            modulePrefs("apps_$pkg").put(AppSP.bypass_packer, !menuItem.isChecked)
+                        R.id.app_toolbar_menu_app_is_protected -> {
+                            prefs("apps_$pkg").edit {  put(AppSP.app_is_protected, !menuItem.isChecked) }
                         }
                         R.id.app_toolbar_menu_configure_in_other_apps -> {
                             startActivity(Intent.createChooser(
--- app/src/main/java/cn/wankkoree/xp/webviewpp/hook/Main.kt
+++ app/src/main/java/cn/wankkoree/xp/webviewpp/hook/Main.kt
@@ -12,7 +12,8 @@ import com.highcapable.yukihookapi.YukiHookAPI
 import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.log.*
 import com.highcapable.yukihookapi.hook.param.PackageParam
-import com.highcapable.yukihookapi.hook.xposed.prefs.YukiHookModulePrefs
+import com.highcapable.yukihookapi.hook.type.android.ContextClass
+import com.highcapable.yukihookapi.hook.xposed.prefs.YukiHookPrefsBridge
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
 
 @InjectYukiHookWithXposed(entryClassName = "Entry", isUsingResourcesHook = false)
@@ -55,130 +56,170 @@ class Main : IYukiHookXposedInit {
                 loggerW(msg = "do not hook webview library: $mProcessName")
                 return@encase // 不 hook WebView 本身
             }
-            val pref = prefs("apps_$mProcessName")
-            if (!pref.get(AppSP.is_enabled)) {
-                loggerI(msg = "$mProcessName hooking not enabled")
-                return@encase // 目标 App 的 Hook 未启用
-            }
+            prefs("apps_$mProcessName").let { pref ->
+                if (!pref.get(AppSP.is_enabled)) {
+                    loggerI(msg = "$mProcessName hooking not enabled")
+                    return@encase // 目标 App 的 Hook 未启用
+                }
 
-            YukiHookLogger.Configs.tag = "WebViewPP<$packageName>"
+                YukiHookLogger.Configs.tag = "WebViewPP<$packageName>"
 
-            loggerI(msg = "hook $mProcessName which run in $processName")
+                loggerI(msg = "hook $mProcessName which run in $processName")
 
-            val mAppClassname = this.appInfo.className
-            if (pref.get(AppSP.bypass_packer) && mAppClassname != null && mAppClassname != "android.app.Application") {
-                loggerI(msg = "Try to get packer's classloader")
-                mAppClassname.hook {
-                    injectMember {
-                        method {
-                            name = "attachBaseContext"
-                            param("android.content.Context")
+                val mAppClassName = this.appInfo.className
+                if (pref.get(AppSP.app_is_protected)) {
+                    loggerI(msg = "try to get classloader of protection")
+                    when (mAppClassName) {
+                        null -> {
+                            // todo 感觉这个情况是不存在的？
+                            loggerE(msg = "the className of applicationInfo is null, get classloader failed")
+                            doHook()
                         }
-                        afterHook {
-                            val context = args(0).any()
-                            if (context != null) {
-                                appClassLoader = (context as Context).classLoader
-                                loggerI(msg = "Get packer's classloader success")
-                            } else {
-                                loggerI(msg = "Get packer's classloader failed! Will use default classloader")
+                        "android.app.Application" -> {
+                            loggerE(msg = "the className of applicationInfo is Application, don't need to replace classloader")
+                            doHook()
+                        }
+                        else -> {
+                            mAppClassName.hook {
+                                injectMember {
+                                    method {
+                                        name = "attachBaseContext"
+                                        param(ContextClass)
+                                    }
+                                    afterHook {
+                                        val context = args(0).cast<Context>()!!
+                                        appClassLoader = context.classLoader
+                                        loggerI(msg = "replace classloader to protection succeed")
+                                        doHook()
+                                    }
+                                }.result {
+                                    onNoSuchMemberFailure {
+                                        loggerE(msg = "Hook.Member.NoSuchMember at preHook\uD83D\uDC49attachBaseContext", e = it)
+                                        doHook()
+                                    }
+                                    onHookingFailure {
+                                        loggerE(msg = "Hook.Member.HookFailure at preHook\uD83D\uDC49attachBaseContext", e = it)
+                                        doHook()
+                                    }
+                                    onHooked {
+                                        loggerI(msg = "Hook.Member.Ended at preHook\uD83D\uDC49attachBaseContext as [$it]")
+                                    }
+                                    onConductFailure { hookParam, it ->
+                                        loggerE(msg = "Hook.Member.ConductFailure at preHook\uD83D\uDC49attachBaseContext(${hookParam.args.joinToString(", ")})", e = it)
+                                        doHook()
+                                    }
+                                }
+                            }.result {
+                                onHookClassNotFoundFailure {
+                                    loggerE(msg = "Hook.Class.NotFound at preHook\uD83D\uDC49$mAppClassName", e = it)
+                                    doHook()
+                                }
+                                onPrepareHook {
+                                    loggerI(msg = "Hook.Class.Started at preHook\uD83D\uDC49$mAppClassName")
+                                }
                             }
-                            doHook(pref)
                         }
                     }
+                } else {
+                    doHook()
                 }
-            } else {
-                doHook(pref)
             }
         }
     }
 
 }
 
-private fun PackageParam.doHook(pref: YukiHookModulePrefs) {
-
-    val cpuArch = with(appInfo.nativeLibraryDir) {
-        when {
-            endsWith("arm64") -> "arm64-v8a"
-            endsWith("arm") -> "armeabi-v7a"
-            else -> {
-                loggerE(msg = "the cpuArch(${toString()}) is not supported")
-                null
+private fun PackageParam.doHook() {
+    prefs("apps_${Main.mProcessName}").let { pref ->
+        val cpuArch = with(appInfo.nativeLibraryDir) {
+            when {
+                endsWith("arm64") -> "arm64-v8a"
+                endsWith("arm") -> "armeabi-v7a"
+                else -> {
+                    loggerE(msg = "the cpuArch(${toString()}) is not supported")
+                    null
+                }
             }
         }
-    }
 
-    loggerI(msg = "loading rules")
+        loggerI(msg = "loading rules")
 
-    if (pref.get(AppSP.debug_mode)) {
-        findWebViewMethods()
-    }
+        if (pref.get(AppSP.debug_mode)) {
+            findWebViewMethods()
+        }
 
-    pref.getSet(AppSP.hooks).forEach { name ->
-        val hookJson = pref.getString("hook_entry_$name", "{}")
-        try {
-            when(val hookMethod = Gson().fromJson(hookJson, HookRules.HookRule::class.java).name) {
-                // TODO: 添加更多 hook 方法
-                "hookWebView" -> {
-                    val hookEntry = Gson().fromJson(hookJson, HookRules.HookRuleWebView::class.java)
-                    hookWebView(
-                        Class_WebView = hookEntry.Class_WebView,
-                        Method_getSettings = hookEntry.Method_getSettings,
-                        Method_setWebContentsDebuggingEnabled = hookEntry.Method_setWebContentsDebuggingEnabled,
-                        Method_setJavaScriptEnabled = hookEntry.Method_setJavaScriptEnabled,
-                        Method_loadUrl = hookEntry.Method_loadUrl,
-                        Method_setWebViewClient = hookEntry.Method_setWebViewClient,
-                    )
-                }
-                "hookWebViewClient" -> {
-                    val hookEntry = Gson().fromJson(hookJson, HookRules.HookRuleWebViewClient::class.java)
-                    hookWebViewClient(
-                        Class_WebViewClient = hookEntry.Class_WebViewClient,
-                        Method_onPageFinished = hookEntry.Method_onPageFinished,
-                        Class_WebView = hookEntry.Class_WebView,
-                        Method_evaluateJavascript = hookEntry.Method_evaluateJavascript,
-                    )
-                }
-                "replaceNebulaUCSDK" -> {
-                    if (cpuArch != null) {
-                        val hookEntry = Gson().fromJson(hookJson, HookRules.ReplaceNebulaUCSDK::class.java)
-                        replaceNebulaUCSDK(
-                            Class_UcServiceSetup = hookEntry.Class_UcServiceSetup,
-                            Method_updateUCVersionAndSdcardPath = hookEntry.Method_updateUCVersionAndSdcardPath,
-                            Field_sInitUcFromSdcardPath = hookEntry.Field_sInitUcFromSdcardPath,
-                            cpuArch = cpuArch,
+        pref.getSet(AppSP.hooks).forEach { name ->
+            val hookJson = pref.getString("hook_entry_$name", "{}")
+            try {
+                when (val hookMethod = Gson().fromJson(hookJson, HookRules.HookRule::class.java).name) {
+                    // TODO: 添加更多 hook 方法
+                    "hookWebView" -> {
+                        val hookEntry = Gson().fromJson(hookJson, HookRules.HookRuleWebView::class.java)
+                        hookWebView(
+                            Class_WebView = hookEntry.Class_WebView,
+                            Method_getSettings = hookEntry.Method_getSettings,
+                            Method_setWebContentsDebuggingEnabled = hookEntry.Method_setWebContentsDebuggingEnabled,
+                            Method_setJavaScriptEnabled = hookEntry.Method_setJavaScriptEnabled,
+                            Method_loadUrl = hookEntry.Method_loadUrl,
+                            Method_setWebViewClient = hookEntry.Method_setWebViewClient,
                         )
                     }
+
+                    "hookWebViewClient" -> {
+                        val hookEntry = Gson().fromJson(hookJson, HookRules.HookRuleWebViewClient::class.java)
+                        hookWebViewClient(
+                            Class_WebViewClient = hookEntry.Class_WebViewClient,
+                            Method_onPageFinished = hookEntry.Method_onPageFinished,
+                            Class_WebView = hookEntry.Class_WebView,
+                            Method_evaluateJavascript = hookEntry.Method_evaluateJavascript,
+                        )
+                    }
+
+                    "replaceNebulaUCSDK" -> {
+                        if (cpuArch != null) {
+                            val hookEntry = Gson().fromJson(hookJson, HookRules.ReplaceNebulaUCSDK::class.java)
+                            replaceNebulaUCSDK(
+                                Class_UcServiceSetup = hookEntry.Class_UcServiceSetup,
+                                Method_updateUCVersionAndSdcardPath = hookEntry.Method_updateUCVersionAndSdcardPath,
+                                Field_sInitUcFromSdcardPath = hookEntry.Field_sInitUcFromSdcardPath,
+                                cpuArch = cpuArch,
+                            )
+                        }
+                    }
+
+                    "hookCrossWalk" -> {
+                        val hookEntry = Gson().fromJson(hookJson, HookRules.HookCrossWalk::class.java)
+                        hookCrossWalk(
+                            Class_XWalkView = hookEntry.Class_XWalkView,
+                            Method_getSettings = hookEntry.Method_getSettings,
+                            Method_setJavaScriptEnabled = hookEntry.Method_setJavaScriptEnabled,
+                            Method_loadUrl = hookEntry.Method_loadUrl,
+                            Method_setResourceClient = hookEntry.Method_setResourceClient,
+                            Class_XWalkPreferences = hookEntry.Class_XWalkPreferences,
+                            Method_setValue = hookEntry.Method_setValue,
+                        )
+                    }
+
+                    "hookXWebView" -> {
+                        val hookEntry = Gson().fromJson(hookJson, HookRules.HookXWebView::class.java)
+                        hookXWebView(
+                            Class_XWebView = hookEntry.Class_XWebView,
+                            Method_initWebviewCore = hookEntry.Method_initWebviewCore,
+                            Method_isXWeb = hookEntry.Method_isXWeb,
+                            Method_isSys = hookEntry.Method_isSys,
+                            Class_XWebPreferences = hookEntry.Class_XWebPreferences,
+                            Method_setValue = hookEntry.Method_setValue,
+                        )
+                    }
+
+                    else -> {
+                        loggerE(msg = "Unknown Hook Method: $hookMethod")
+                    }
                 }
-                "hookCrossWalk" -> {
-                    val hookEntry = Gson().fromJson(hookJson, HookRules.HookCrossWalk::class.java)
-                    hookCrossWalk(
-                        Class_XWalkView = hookEntry.Class_XWalkView,
-                        Method_getSettings = hookEntry.Method_getSettings,
-                        Method_setJavaScriptEnabled = hookEntry.Method_setJavaScriptEnabled,
-                        Method_loadUrl = hookEntry.Method_loadUrl,
-                        Method_setResourceClient = hookEntry.Method_setResourceClient,
-                        Class_XWalkPreferences = hookEntry.Class_XWalkPreferences,
-                        Method_setValue = hookEntry.Method_setValue,
-                    )
-                }
-                "hookXWebView" -> {
-                    val hookEntry = Gson().fromJson(hookJson, HookRules.HookXWebView::class.java)
-                    hookXWebView(
-                        Class_XWebView = hookEntry.Class_XWebView,
-                        Method_initWebviewCore = hookEntry.Method_initWebviewCore,
-                        Method_isXWeb = hookEntry.Method_isXWeb,
-                        Method_isSys = hookEntry.Method_isSys,
-                        Class_XWebPreferences = hookEntry.Class_XWebPreferences,
-                        Method_setValue = hookEntry.Method_setValue,
-                    )
-                }
-                else -> {
-                    loggerE(msg = "Unknown Hook Method: $hookMethod")
-                }
+            } catch (e: Exception) {
+                loggerE(msg = "Parse Failed!", e = e)
+                return@forEach // continue
             }
-        } catch (e : Exception) {
-            loggerE(msg = "Parse Failed!", e = e)
-            return@forEach // continue
         }
     }
 }
